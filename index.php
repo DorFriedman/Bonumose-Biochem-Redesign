179b7050b3021764f9f47d8de317899790fb0b53
Dor Friedman <df9sv@virginia.edu>
2017-08-01 02:19:18 -0400

    made the functionality of the 7 sections

--- index.php
+++ index.php
@@ -12,45 +12,135 @@
  * @package bonumose-theme
  */
 
-get_header(); ?>
+get_header();
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<ul>
+	<li style="float:right" class="donate"><button class="donateButton">DONATE</button></li>
+	<li style="float:right" class="dropdown"><button class="contactButton">Contact</button></li>
+</ul>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+<!-- Left Side -->
+<div class="bigCont side-l">
+	<div class="smallCont cont1"><button class="contButton but1">Hi</button></div>
+	<div class="smallCont cont2"><button class="contButton but2">Hello</button></div>
+	<div class="smallCont cont3"><button class="contButton but3">Hey there</button></div>
+	<div class="smallCont cont4"><button class="contButton but4">Hey there</button></div>
+	<div class="smallCont cont5"><button class="contButton but5">Hey there</button></div>
+	<div class="smallCont cont6"><button class="contButton but6">Hey there</button></div>
+	<div class="smallCont cont7"><button class="contButton but7">Hey there</button></div>
+</div>
 
-			<?php
-			endif;
+<div class="bigCont textLeft">
+	<div class="smallCont text1"></div>
+	<div class="smallCont text2"></div>
+	<div class="smallCont text3"></div>
+	<div class="smallCont text4"></div>
+	<div class="smallCont text5"></div>
+	<div class="smallCont text6"></div>
+	<div class="smallCont text7"></div>
+</div>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
+<!-- Right Side -->
+<div class="bigCont side-r"></div>
 
-			the_posts_navigation();
 
-		else :
+<script>
+$('.but1').click(function(){
+    if($(".text1").hasClass('moved')){
+        $(".text1").removeClass('moved');
+    } else {
+        $(".text1").addClass('moved');
+        $(".text2").removeClass('moved');
+        $(".text3").removeClass('moved');
+        $(".text4").removeClass('moved');
+        $(".text5").removeClass('moved');
+        $(".text6").removeClass('moved');
+        $(".text7").removeClass('moved');
+    }
+});
+$('.but2').click(function(){
+    if($(".text2").hasClass('moved')){
+        $(".text2").removeClass('moved');
+    } else {
+        $(".text2").addClass('moved');
+        $(".text1").removeClass('moved');
+        $(".text3").removeClass('moved');
+        $(".text4").removeClass('moved');
+        $(".text5").removeClass('moved');
+        $(".text6").removeClass('moved');
+        $(".text7").removeClass('moved');
+    }
+});
+$('.but3').click(function(){
+    if($(".text3").hasClass('moved')){
+        $(".text3").removeClass('moved');
+    } else {
+        $(".text3").addClass('moved');
+        $(".text1").removeClass('moved');
+        $(".text2").removeClass('moved');
+        $(".text4").removeClass('moved');
+        $(".text5").removeClass('moved');
+        $(".text6").removeClass('moved');
+        $(".text7").removeClass('moved');
+    }
+});
+$('.but4').click(function(){
+    if($(".text4").hasClass('moved')){
+        $(".text4").removeClass('moved');
+    } else {
+        $(".text4").addClass('moved');
+        $(".text1").removeClass('moved');
+        $(".text2").removeClass('moved');
+        $(".text3").removeClass('moved');
+        $(".text5").removeClass('moved');
+        $(".text6").removeClass('moved');
+        $(".text7").removeClass('moved');
+    }
+});
+$('.but5').click(function(){
+    if($(".text5").hasClass('moved')){
+        $(".text5").removeClass('moved');
+    } else {
+        $(".text5").addClass('moved');
+        $(".text1").removeClass('moved');
+        $(".text2").removeClass('moved');
+        $(".text4").removeClass('moved');
+        $(".text3").removeClass('moved');
+        $(".text6").removeClass('moved');
+        $(".text7").removeClass('moved');
+    }
+});
+$('.but6').click(function(){
+    if($(".text6").hasClass('moved')){
+        $(".text6").removeClass('moved');
+    } else {
+        $(".text6").addClass('moved');
+        $(".text1").removeClass('moved');
+        $(".text2").removeClass('moved');
+        $(".text4").removeClass('moved');
+        $(".text5").removeClass('moved');
+        $(".text3").removeClass('moved');
+        $(".text7").removeClass('moved');
+    }
+});
+$('.but7').click(function(){
+    if($(".text7").hasClass('moved')){
+        $(".text7").removeClass('moved');
+    } else {
+        $(".text7").addClass('moved');
+        $(".text1").removeClass('moved');
+        $(".text2").removeClass('moved');
+        $(".text4").removeClass('moved');
+        $(".text5").removeClass('moved');
+        $(".text6").removeClass('moved');
+        $(".text3").removeClass('moved');
+    }
+});
+</script>
 
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
